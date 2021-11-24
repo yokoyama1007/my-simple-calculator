e87dcc05c6b45170e027933d465ed63c4df83fcf
k.yokoyama <yokoyama@anysense.co.jp>
2021-11-24 18:01:48 +0900

    init

---
+++ my-simple-calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  my-simple-calculator
+//
+//  Created by 横山　賢一 on 2021/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
